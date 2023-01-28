a48ff82ac6b69fa5b69413147d51286ef78713d9
DESKTOP-DNAJ88G\WPOSS <juan02gt@gmail.com>
2023-01-28 08:01:35 -0500

    vsdv

--- app/src/main/java/com/example/myapi/interfas/ApiPeliculas.java
+++ app/src/main/java/com/example/myapi/interfas/ApiPeliculas.java
@@ -15,24 +15,23 @@ import retrofit2.http.Query;
 public interface ApiPeliculas {
 
 
-
     @GET("3/movie/popular")
     Call<results> leerPeliculas(@Query("api_key") String api_key, @Query("page") int page,
-                                @Query("language")String language);
+                                @Query("language") String language);
 
-@GET("3/search/movie")
-Call<results> leerBusqueda(@Query("api_key") String api_key,
-                                      @Query("language")String language,
-                                      @Query("query") String query);
+    @GET("3/search/movie")
+    Call<results> leerBusqueda(@Query("api_key") String api_key,
+                               @Query("language") String language,
+                               @Query("query") String query);
 
     @GET("3/movie/upcoming")
     Call<results> leerpopular(@Query("api_key") String api_key, @Query("page") int page,
-                                    @Query("language")String language);
+                              @Query("language") String language);
 
 
     @GET("3/movie/top_rated")
     Call<results> leermastop(@Query("api_key") String api_key, @Query("page") int page,
-                              @Query("language")String language);
+                             @Query("language") String language);
 
 
 }
